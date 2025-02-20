17d100ddd0419f626df1b539b1790dbcea94371b
M.Bagheri <MahdiBagheriVar@gmail.com>
2025-02-20 10:53:58 +0330

    fix: Add check to skip fullText index creation for SQLite database

--- database/migrations/2025_02_20_064617_convert_to_fulltext_in_articles.php
+++ database/migrations/2025_02_20_064617_convert_to_fulltext_in_articles.php
@@ -11,14 +11,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('articles', static function (Blueprint $table) {
-            $table->fullText(['title', 'description', 'content'], 'articles_search_fulltext');
+            if (DB::getDriverName() !== 'sqlite') {
+                $table->fullText(['title', 'description', 'content'], 'articles_search_fulltext');
+            }
         });
     }
 
     public function down(): void
     {
         Schema::table('articles', static function (Blueprint $table) {
-            $table->dropFullText('articles_search_fulltext');
+            if (DB::getDriverName() !== 'sqlite') {
+                $table->dropFullText('articles_search_fulltext');
+            }
         });
     }
 };
